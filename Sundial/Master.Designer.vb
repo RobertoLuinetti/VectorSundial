ade7bea3f571d80a7b84a3587e8f7a3d61dd45ec
Roberto Luinetti <luinetti@hotmail.com>
2020-05-03 23:03:19 +0200

    Aggiungere i file di progetto.

---
+++ Sundial/Master.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Master
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonRUN = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LongQ = New System.Windows.Forms.TextBox()
+        Me.LatQ = New System.Windows.Forms.TextBox()
+        Me.LongS = New System.Windows.Forms.TextBox()
+        Me.LatS = New System.Windows.Forms.TextBox()
+        Me.LongP = New System.Windows.Forms.TextBox()
+        Me.LongG = New System.Windows.Forms.TextBox()
+        Me.LatP = New System.Windows.Forms.TextBox()
+        Me.LatG = New System.Windows.Forms.TextBox()
+        Me.PicGraph = New System.Windows.Forms.PictureBox()
+        Me.ButtonDXF = New System.Windows.Forms.Button()
+        Me.Declinazione = New System.Windows.Forms.TrackBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Linee_Ore = New System.Windows.Forms.RadioButton()
+        Me.Linee_MezzeOre = New System.Windows.Forms.RadioButton()
+        Me.Linee_QuartiOra = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.PicGraph, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Declinazione, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonRUN
+        '
+        Me.ButtonRUN.Location = New System.Drawing.Point(182, 12)
+        Me.ButtonRUN.Name = "ButtonRUN"
+        Me.ButtonRUN.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonRUN.TabIndex = 0
+        Me.ButtonRUN.Text = "RUN"
+        Me.ButtonRUN.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "LONG"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 26
+        Me.Label1.Text = "LAT"
+        '
+        'LongQ
+        '
+        Me.LongQ.Location = New System.Drawing.Point(146, 38)
+        Me.LongQ.Name = "LongQ"
+        Me.LongQ.Size = New System.Drawing.Size(13, 20)
+        Me.LongQ.TabIndex = 25
+        Me.LongQ.Text = "E"
+        Me.LongQ.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LatQ
+        '
+        Me.LatQ.Location = New System.Drawing.Point(146, 12)
+        Me.LatQ.Name = "LatQ"
+        Me.LatQ.Size = New System.Drawing.Size(13, 20)
+        Me.LatQ.TabIndex = 24
+        Me.LatQ.Text = "N"
+        Me.LatQ.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LongS
+        '
+        Me.LongS.Location = New System.Drawing.Point(105, 38)
+        Me.LongS.Name = "LongS"
+        Me.LongS.Size = New System.Drawing.Size(35, 20)
+        Me.LongS.TabIndex = 23
+        Me.LongS.Text = "28,4"
+        Me.LongS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LatS
+        '
+        Me.LatS.Location = New System.Drawing.Point(105, 12)
+        Me.LatS.Name = "LatS"
+        Me.LatS.Size = New System.Drawing.Size(35, 20)
+        Me.LatS.TabIndex = 22
+        Me.LatS.Text = "42,7"
+        Me.LatS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LongP
+        '
+        Me.LongP.Location = New System.Drawing.Point(80, 38)
+        Me.LongP.Name = "LongP"
+        Me.LongP.Size = New System.Drawing.Size(19, 20)
+        Me.LongP.TabIndex = 21
+        Me.LongP.Text = "4"
+        Me.LongP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LongG
+        '
+        Me.LongG.Location = New System.Drawing.Point(55, 38)
+        Me.LongG.Name = "LongG"
+        Me.LongG.Size = New System.Drawing.Size(19, 20)
+        Me.LongG.TabIndex = 20
+        Me.LongG.Text = "9"
+        Me.LongG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LatP
+        '
+        Me.LatP.Location = New System.Drawing.Point(80, 12)
+        Me.LatP.Name = "LatP"
+        Me.LatP.Size = New System.Drawing.Size(19, 20)
+        Me.LatP.TabIndex = 19
+        Me.LatP.Text = "35"
+        Me.LatP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LatG
+        '
+        Me.LatG.Location = New System.Drawing.Point(55, 12)
+        Me.LatG.Name = "LatG"
+        Me.LatG.Size = New System.Drawing.Size(19, 20)
+        Me.LatG.TabIndex = 18
+        Me.LatG.Text = "45"
+        Me.LatG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'PicGraph
+        '
+        Me.PicGraph.Location = New System.Drawing.Point(12, 95)
+        Me.PicGraph.Name = "PicGraph"
+        Me.PicGraph.Size = New System.Drawing.Size(960, 540)
+        Me.PicGraph.TabIndex = 28
+        Me.PicGraph.TabStop = False
+        '
+        'ButtonDXF
+        '
+        Me.ButtonDXF.Location = New System.Drawing.Point(278, 12)
+        Me.ButtonDXF.Name = "ButtonDXF"
+        Me.ButtonDXF.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonDXF.TabIndex = 29
+        Me.ButtonDXF.Text = "DXF"
+        Me.ButtonDXF.UseVisualStyleBackColor = True
+        '
+        'Declinazione
+        '
+        Me.Declinazione.Location = New System.Drawing.Point(415, 38)
+        Me.Declinazione.Maximum = 180
+        Me.Declinazione.Name = "Declinazione"
+        Me.Declinazione.Size = New System.Drawing.Size(104, 45)
+        Me.Declinazione.TabIndex = 30
+        Me.Declinazione.TickFrequency = 30
+        Me.Declinazione.Value = 90
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(417, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Declinazione Parete"
+        '
+        'Linee_Ore
+        '
+        Me.Linee_Ore.AutoSize = True
+        Me.Linee_Ore.Location = New System.Drawing.Point(16, 19)
+        Me.Linee_Ore.Name = "Linee_Ore"
+        Me.Linee_Ore.Size = New System.Drawing.Size(53, 17)
+        Me.Linee_Ore.TabIndex = 32
+        Me.Linee_Ore.TabStop = True
+        Me.Linee_Ore.Text = "Orario"
+        Me.Linee_Ore.UseVisualStyleBackColor = True
+        '
+        'Linee_MezzeOre
+        '
+        Me.Linee_MezzeOre.AutoSize = True
+        Me.Linee_MezzeOre.Location = New System.Drawing.Point(16, 42)
+        Me.Linee_MezzeOre.Name = "Linee_MezzeOre"
+        Me.Linee_MezzeOre.Size = New System.Drawing.Size(74, 17)
+        Me.Linee_MezzeOre.TabIndex = 33
+        Me.Linee_MezzeOre.TabStop = True
+        Me.Linee_MezzeOre.Text = "Mezze ore"
+        Me.Linee_MezzeOre.UseVisualStyleBackColor = True
+        '
+        'Linee_QuartiOra
+        '
+        Me.Linee_QuartiOra.AutoSize = True
+        Me.Linee_QuartiOra.Location = New System.Drawing.Point(16, 65)
+        Me.Linee_QuartiOra.Name = "Linee_QuartiOra"
+        Me.Linee_QuartiOra.Size = New System.Drawing.Size(79, 17)
+        Me.Linee_QuartiOra.TabIndex = 34
+        Me.Linee_QuartiOra.TabStop = True
+        Me.Linee_QuartiOra.Text = "Quarti d'ora"
+        Me.Linee_QuartiOra.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Linee_Ore)
+        Me.GroupBox1.Controls.Add(Me.Linee_QuartiOra)
+        Me.GroupBox1.Controls.Add(Me.Linee_MezzeOre)
+        Me.GroupBox1.Location = New System.Drawing.Point(592, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(126, 89)
+        Me.GroupBox1.TabIndex = 35
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Dettaglio temporale"
+        '
+        'Master
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(984, 647)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Declinazione)
+        Me.Controls.Add(Me.ButtonDXF)
+        Me.Controls.Add(Me.PicGraph)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LongQ)
+        Me.Controls.Add(Me.LatQ)
+        Me.Controls.Add(Me.LongS)
+        Me.Controls.Add(Me.LatS)
+        Me.Controls.Add(Me.LongP)
+        Me.Controls.Add(Me.LongG)
+        Me.Controls.Add(Me.LatP)
+        Me.Controls.Add(Me.LatG)
+        Me.Controls.Add(Me.ButtonRUN)
+        Me.Name = "Master"
+        Me.Text = "Form1"
+        CType(Me.PicGraph, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Declinazione, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonRUN As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LongQ As TextBox
+    Friend WithEvents LatQ As TextBox
+    Friend WithEvents LongS As TextBox
+    Friend WithEvents LatS As TextBox
+    Friend WithEvents LongP As TextBox
+    Friend WithEvents LongG As TextBox
+    Friend WithEvents LatP As TextBox
+    Friend WithEvents LatG As TextBox
+    Friend WithEvents PicGraph As PictureBox
+    Friend WithEvents ButtonDXF As Button
+    Friend WithEvents Declinazione As TrackBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Linee_Ore As RadioButton
+    Friend WithEvents Linee_MezzeOre As RadioButton
+    Friend WithEvents Linee_QuartiOra As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Sundial/Master.vb
@@ -0,0 +1,152 @@
+﻿Public Class Master
+
+    Dim tick_ora As Double = 1
+
+    Private Sub ButtonRUN_Click(sender As Object, e As EventArgs) Handles ButtonRUN.Click
+        RenderGraph()
+    End Sub
+
+    Public Sub RenderGraph()
+        Me.Cursor = Cursors.WaitCursor
+        Dim bm As New Bitmap(PicGraph.ClientSize.Width, PicGraph.ClientSize.Height)
+        Using gr As Graphics = Graphics.FromImage(bm)
+            gr.Clear(Color.Black)
+
+            Dim latitudine As Double = 45 + (35 / 60) + (42 / 3600)
+            Dim longitudine As Double = 9 + (4 / 60) + (28 / 3600)
+            Dim azimut As Double = Declinazione.Value
+            Dim gnomone As Double = 20
+            Dim x_old_clip As Double = 0
+            Dim y_old_clip As Double = 0
+            Dim dr As Double = Math.PI / 180
+            Dim elevazione_max As Double = 23.27 * dr
+
+            latitudine = latitudine * dr
+            longitudine = longitudine * dr
+            azimut = azimut * dr
+            Dim cliplimit As Double = 2000
+            Dim hoff As Double = 0
+            Dim voff As Double = -2
+            Dim tick As Double = elevazione_max * 2 / 3600
+            'tick = tick / 50
+            Dim d As Double = 1
+            Dim latoy As Double = 5
+            Dim ratio As Double = 133 / 100
+            Dim latox As Double = latoy * ratio
+            SetDisplayPhysicalArea(PicGraph, 960, 540, 20, 20, 665, 500)
+
+
+            Dim xl As Double = (-latox + hoff)
+            Dim xr As Double = (+latox + hoff)
+            Dim yb As Double = (-latoy + voff)
+            Dim yt As Double = (+latoy + voff)
+            Dim ofc As Double = latoy / 50
+            SetScale(xl, xr, yb, yt)
+            'Draw(gr, PenCyan, xl, yb, xr, yt)
+            Frame(gr, PenGreen)
+            '           Axes(1, 1, 10, yt); axes(1,1,10,yb); axes(1,1,xl,10); axes(1,1,xr,10);
+            '{    axes(1,1,xl,yt); axes(1,1,xr,yb);}
+            '    initdxf('sun');
+            '    clipdxfplot(xl, yb); clipdxfplot(xr, yb); clipdxfplot(xr,yt);
+            '    clipdxfplot(xl, yt); clipdxfplot(xl,yb); penupdxf;    
+            Dim bgy As Double = Math.Tan(latitudine) / Math.Sin(azimut)
+            Dim bgx As Double
+            If azimut <> (90 * dr) Then
+                bgx = 1 / Math.Tan(azimut)
+            Else
+                bgx = 0
+            End If
+            Draw(gr, PenCyan, bgx - ofc, bgy, bgx + ofc, bgy)
+            Draw(gr, PenCyan, bgx, bgy - ofc, bgx, bgy - ofc + 2 * ofc)
+            Draw(gr, PenCyan, -ofc, 0, +ofc, 0)
+            Draw(gr, PenCyan, 0, -ofc, 0, +ofc)
+
+            Dim proiezione_gnomone As Double = Math.Sqrt(bgx * bgx + bgy * bgy)
+            Dim gnomone_perpendicolare As Double = (proiezione_gnomone / Math.Sin(Math.PI / 2 - (Math.PI / 2 - latitudine))) * Math.Sin(Math.PI / 2 - latitudine)
+            Dim gnomone_obliquo As Double = Math.Sqrt(proiezione_gnomone * proiezione_gnomone + gnomone_perpendicolare * gnomone_perpendicolare)
+
+
+
+            Dim ascensione As Double
+            Dim elevazione As Double
+            Dim xold, yold, x, y As Double
+            Dim xx, yy As Double
+            Dim a1, a2, a3 As Double
+            For ora = 6 To 18 Step tick_ora
+                ascensione = longitudine + 15 * (ora - 12) * dr
+                xold = x
+                yold = y
+                For elevazione = -elevazione_max To elevazione_max Step elevazione_max / 1200
+
+                    'astro begin
+                    a1 = Math.Sin(elevazione) * Math.Sin(latitudine) + Math.Cos(elevazione) * Math.Cos(latitudine) * Math.Cos(ascensione)
+                    a1 = Math.Atan(a1 / Math.Sqrt(1 - a1 * a1))
+
+                    If ascensione = 0 Then
+                        a2 = Math.PI
+                    Else
+                        a2 = (Math.Sin(elevazione) * Math.Cos(latitudine) - Math.Cos(elevazione) * Math.Sin(latitudine) * Math.Cos(ascensione)) / Math.Cos(a1)
+                        a2 = Math.PI / 2 - Math.Atan(a2 / Math.Sqrt(1 - a2 * a2))
+                        If ascensione > 0 Then a2 = 2 * Math.PI - a2
+                    End If
+
+                    a3 = a2 - azimut - Math.PI / 2
+                    x = Math.Tan(a3)
+                    y = -1 * Math.Tan(a1) / Math.Cos(a3)
+                    'astro end
+                    If (x > xold + d) Or (x < xold - d) Or (y > yold + d) Or (y < yold - d) Then
+                        xx = x
+                        yy = y
+                    End If
+                    If ora = Math.Truncate(ora) Then
+                        Draw(gr, PenYellow, xx, yy, x, y)
+                    Else
+                        Draw(gr, PenCyan, xx, yy, x, y)
+                    End If
+
+                    'Draw(gr, PenCyan, xx, yy, x, y)
+                    'Plot(gr, Color.Red, x, y)
+                    xold = x
+                    yold = y
+                Next elevazione
+            Next ora
+
+        End Using
+
+        PicGraph.Image = bm
+
+        Me.Cursor = Cursors.Default
+
+        'If BMPSave = True Then
+        '    Clipboard.SetDataObject(DirectCast(picGraph.Image.Clone, Bitmap), True)
+        '    InFile = Replace(InFile, ".gpx", ".bmp")
+        '    bm.Save(InFile, System.Drawing.Imaging.ImageFormat.Bmp)
+        '    BMPSave = False
+        'End If
+    End Sub
+
+    Private Sub Master_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        Me.Linee_Ore.Checked = True
+    End Sub
+
+
+    Private Sub Declinazione_Scroll(sender As Object, e As EventArgs) Handles Declinazione.Scroll
+        RenderGraph()
+    End Sub
+
+    Private Sub Linee_Ore_CheckedChanged(sender As Object, e As EventArgs) Handles Linee_Ore.CheckedChanged
+        tick_ora = 1
+        RenderGraph()
+    End Sub
+
+    Private Sub Linee_MezzeOre_CheckedChanged(sender As Object, e As EventArgs) Handles Linee_MezzeOre.CheckedChanged
+        tick_ora = 0.5
+        RenderGraph()
+    End Sub
+
+    Private Sub Linee_QuartiOra_CheckedChanged(sender As Object, e As EventArgs) Handles Linee_QuartiOra.CheckedChanged
+        tick_ora = 0.25
+        RenderGraph()
+    End Sub
+End Class
